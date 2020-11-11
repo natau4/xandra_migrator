d83d450f51f2e6de77039d00baf0d2c79ced1588
Azat Zinnyurov <a.zinnyurov@nomera.com>
2020-11-11 10:40:02 +0300

    Initial commit

---
+++ lib/xandra_migrator.ex
@@ -0,0 +1,18 @@
+defmodule XandraMigrator do
+  @moduledoc """
+  Documentation for `XandraMigrator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XandraMigrator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xandra_migrator_test.exs
@@ -0,0 +1,8 @@
+defmodule XandraMigratorTest do
+  use ExUnit.Case
+  doctest XandraMigrator
+
+  test "greets the world" do
+    assert XandraMigrator.hello() == :world
+  end
+end
