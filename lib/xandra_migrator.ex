89a603c2fe1a9ec49c4bdc82272ca0f7763a936b
Azat Zinnyurov <a.zinnyurov@nomera.com>
2021-01-20 12:56:53 +0300

    Fix config reading

--- lib/xandra_migrator.ex
+++ lib/xandra_migrator.ex
@@ -4,12 +4,8 @@ defmodule XandraMigrator do
   alias XandraMigrator.SchemaMigrations
   alias XandraMigrator.XandraModels.SchemaMigration
 
-  def parrent_app_name() do
-    Mix.Project.config()[:app]
-  end
-
   def cassandra_keyspace() do
-    parrent_app_name() |> Application.get_env(:xandra) |> Keyword.get(:keyspace)
+    Application.get_env(:xandra_launcher, :xandra) |> Keyword.get(:keyspace)
   end
 
   def migrate() do
@@ -35,21 +31,6 @@ defmodule XandraMigrator do
     if Enum.empty?(new_migration_files), do: IO.puts("No one new migration was found.")
 
     run_migration(new_migration_files)
-  end
-
-  defp cassandra_params() do
-    params = Application.fetch_env!(:deep_link_service, :xandra)
-
-    fun = &Xandra.execute(&1, "USE #{params[:keyspace]}")
-
-    [
-      name: :xandra,
-      pool_size: params[:pool_size],
-      nodes: [params[:host]],
-      after_connect: fun,
-      queue_target: params[:queue_target],
-      queue_interval: params[:queue_interval]
-    ]
   end
 
   defp migrations_path!() do
